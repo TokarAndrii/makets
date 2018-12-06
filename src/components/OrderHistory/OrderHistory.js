5ce6f741251e855b1e7beb4266a53392f1f0e97c
tokarandrii <tokar.andrii@gmail.com>
2018-12-06 14:59:56 +0200

    make hiden from unauthorized users remove btn at orderHistory

--- src/components/OrderHistory/OrderHistory.js
+++ src/components/OrderHistory/OrderHistory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '../shared/Button/Button';
+import { AuthContext } from '../../contexts/AuthContext';
 import styles from './OrderHistory.module.css';
 
 const OrderHistory = ({
@@ -10,51 +11,57 @@ const OrderHistory = ({
   children,
   disableDetailsBtn,
 }) => (
-  <div>
-    <table className={className}>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Date</th>
-          <th>Price</th>
-          <th>Address of Delivery</th>
-          <th>Raiting</th>
-          <th style={{ width: '600px' }}>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {orderList.map((order, index) => {
-          const currNumber = index;
-          return (
-            <tr key={order.id}>
-              <td>{currNumber}</td>
-              <td>{order.date}</td>
-              <td>{order.price}</td>
-              <td>{order.address}</td>
-              <td>{order.rating}</td>
-              <td>
-                {!disableDetailsBtn ? (
-                  <Button
-                    type="button"
-                    text="Details"
-                    className={styles.OrderHistotyDetailsBtn}
-                    onClick={() => onShowDetails(order.id)}
-                  />
-                ) : null}
-                <Button
-                  type="button"
-                  text="Remove"
-                  className={styles.OrderHistotyRmBtn}
-                  onClick={() => onDelete(order.id)}
-                />
-              </td>
+  <AuthContext.Consumer>
+    {({ isAuthenticated }) => (
+      <div>
+        <table className={className}>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Date</th>
+              <th>Price</th>
+              <th>Address of Delivery</th>
+              <th>Raiting</th>
+              <th style={{ width: '600px' }}>Actions</th>
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
-    <div>{children}</div>
-  </div>
+          </thead>
+          <tbody>
+            {orderList.map((order, index) => {
+              const currNumber = index;
+              return (
+                <tr key={order.id}>
+                  <td>{currNumber}</td>
+                  <td>{order.date}</td>
+                  <td>{order.price}</td>
+                  <td>{order.address}</td>
+                  <td>{order.rating}</td>
+                  <td>
+                    {!disableDetailsBtn ? (
+                      <Button
+                        type="button"
+                        text="Details"
+                        className={styles.OrderHistotyDetailsBtn}
+                        onClick={() => onShowDetails(order.id)}
+                      />
+                    ) : null}
+                    {isAuthenticated && (
+                      <Button
+                        type="button"
+                        text="Remove"
+                        className={styles.OrderHistotyRmBtn}
+                        onClick={() => onDelete(order.id)}
+                      />
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <div>{children}</div>
+      </div>
+    )}
+  </AuthContext.Consumer>
 );
 
 export default OrderHistory;
