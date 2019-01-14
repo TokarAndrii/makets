736313e96a174b0ab20f59a2c645609df6d974db
TokarAndrii <tokar.andrii@gmail.com>
2019-01-14 20:56:52 +0200

    clean code at menuSelectors

--- src/pages/menu/duck/menuSelectors.js
+++ src/pages/menu/duck/menuSelectors.js
@@ -4,15 +4,6 @@ const getAllMenuList = state => state.menuList;
 
 const getMenuFilter = state => state.menuFilter;
 
-// const getFilteredMenuList = state => {
-//   const menuList = getAllMenuList(state);
-//   const filter = getMenuFilter(state);
-
-//   return menuList.filter(menuItem =>
-//     menuItem.name.toLowerCase().includes(filter.toLowerCase()),
-//   );
-// };
-
 const getFilteredMenuList = createSelector(
   [getAllMenuList, getMenuFilter],
   (menuList, filter) =>
